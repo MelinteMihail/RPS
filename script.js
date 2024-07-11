836d011db56b2fff154621fb1e4be6ccb05a7803
Mihail <mihailmelinte74@gmail.com>
2024-07-11 11:32:34 +0300

    Finish logic

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
-
 let winnerText = document.querySelector(".winner-sect");
 
 document.addEventListener("DOMContentLoaded", () => {
-    winnerText.style.display = "none";
-})
+    winnerText.style.visibility = "hidden";
+});
 
 function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
@@ -19,114 +18,157 @@ function getComputerChoice() {
 const userScoreDisplay = document.querySelector(".user-score .update");
 const computerScoreDisplay = document.querySelector(".computer-score .update");
 
-let humanIcon = document.querySelector(".box1 .icon-container i")
-let computerIcon = document.querySelector(".box3 .icon-container.fa-flip-horizontal i")
+let humanIcon = document.querySelector(".box1 .icon-container i");
+let computerIcon = document.querySelector(".box3 .icon-container.fa-flip-horizontal i");
 
 
+let winner = document.querySelector(".final-winner");
 
-function playGame(){
-    let rounds = 0;
-    let humanScore = 0;
-    let computerScore = 0;
-    winnerText.style.display = "block";
-    let finalResult = document.querySelector(".round-winner");
+let humanScore = 0;
+let computerScore = 0;
+let finalResult = document.querySelector(".round-winner");
+let rounds = 0;
+const totalRounds = 5;
 
-    function playRound(humanChoice) {
-        let computerChoice = getComputerChoice();
-        // const statement = `Computer picked ${computerChoice}, Human picked ${humanChoice}`;
-    
-        if(humanChoice === "Rock") {
-            humanIcon.className = "fa-solid fa-hand-fist fa-rotate-90";
-    
-            if(computerChoice === "Rock") {
-                finalResult.textContent = "It's a tie!";
-                computerIcon.className = "fa-solid fa-hand-fist fa-rotate-90"
-                
-            } else if(computerChoice === "Paper") {
-                finalResult.textContent = "You lose! Paper beats Rock!";
-                computerIcon.className = "fa-solid fa-hand";
-                updateComputerScore();
-            } else {
-                finalResult.textContent = "You win! Rock beats Scissors!";
-                updateUserScore();
-                computerIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
-            }
-        } else if(humanChoice === "Scissors") {
-            humanIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
-    
-            if(computerChoice === "Rock") {
-                finalResult.textContent = "You lose! Rock beats Scissors";
-                computerIcon.className = "fa-solid fa-hand-fist fa-rotate-90"
-                updateComputerScore();
-            } else if(computerChoice === "Paper") {
-                finalResult.textContent = "You win! Scissors beats Paper!";
-                computerIcon.className = "fa-solid fa-hand";
-                updateUserScore();
-            } else {
-                finalResult.textContent = "It's a tie!";
-                computerIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
-            }
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    winnerText.style.visibility = "visible";
+
+    if(humanChoice === "Rock") {
+        humanIcon.className = "fa-solid fa-hand-fist fa-rotate-90";
+
+        if(computerChoice === "Rock") {
+            finalResult.textContent = "It's a tie!";
+            computerIcon.className = "fa-solid fa-hand-fist fa-rotate-90"
+            
+        } else if(computerChoice === "Paper") {
+            finalResult.textContent = "You lose! Paper beats Rock!";
+            computerIcon.className = "fa-solid fa-hand";
+            updateComputerScore();
         } else {
-            humanIcon.className = "fa-solid fa-hand";
-    
-            if(computerChoice === "Rock") {
-                finalResult.textContent = "You win! Paper beats Rock";
-                computerIcon.className = "fa-solid fa-hand-fist fa-rotate-90"
-                updateUserScore();
-            } else if(computerChoice === "Paper") {
-                finalResult.textContent = "It's a tie!";
-                computerIcon.className = "fa-solid fa-hand";
-            } else {
-                finalResult.textContent = "You lose! Scissors beats Paper";
-                updateComputerScore();
-                computerIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
-            }
+            finalResult.textContent = "You win! Rock beats Scissors!";
+            updateUserScore();
+            computerIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
+        }
+    } else if(humanChoice === "Scissors") {
+        humanIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
+
+        if(computerChoice === "Rock") {
+            finalResult.textContent = "You lose! Rock beats Scissors";
+            computerIcon.className = "fa-solid fa-hand-fist fa-rotate-90"
+            updateComputerScore();
+        } else if(computerChoice === "Paper") {
+            finalResult.textContent = "You win! Scissors beats Paper!";
+            computerIcon.className = "fa-solid fa-hand";
+            updateUserScore();
+        } else {
+            finalResult.textContent = "It's a tie!";
+            computerIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
+        }
+    } else {
+        humanIcon.className = "fa-solid fa-hand";
+
+        if(computerChoice === "Rock") {
+            finalResult.textContent = "You win! Paper beats Rock";
+            computerIcon.className = "fa-solid fa-hand-fist fa-rotate-90"
+            updateUserScore();
+        } else if(computerChoice === "Paper") {
+            finalResult.textContent = "It's a tie!";
+            computerIcon.className = "fa-solid fa-hand";
+        } else {
+            finalResult.textContent = "You lose! Scissors beats Paper";
+            updateComputerScore();
+            computerIcon.className = "fa-solid fa-hand-scissors fa-flip-horizontal";
         }
     }
 
-    rockBtn.addEventListener("click", () => {
-        playRound("Rock");
-    });
-
-    paperBtn.addEventListener("click", () => {
-        playRound("Paper");
-    });
-
-    scissorsBtn.addEventListener("click", () => {
-        playRound("Scissors");
-    });
-
-    while(rounds < 5) {
-        playRound();
-        rounds++;
+    rounds++;
+    if(rounds === totalRounds) {
+        if(computerScore > humanScore) {
+            winner.textContent = `You lost! Computer won more rounds than you!`;
+        } else if(humanScore > computerScore){
+            winner.textContent = `You won! You won more rounds than the computer!`;
+        } else {
+            winner.textContent = `It's a tie! You won the same amount of rounds as the computer!`;
+        }
+        finalResult.textContent = "";
+        disableButtons();
+        setTimeout(() => {
+            resetScore();
+        }, 1000);
     }
+}
 
-    let winner = document.querySelector(".final-winner");
+rockBtn.addEventListener("click", () => {
+    playRound("Rock");
+});
 
-    if(computerScore > humanScore) {
-        winner.textContent = `You lost!\nComputer won more rounds than you!`;
-        finalResult.textContent = "";
-    } else if(humanScore > computerScore){
-        winner.textContent = `You won!\nYou won more rounds than the computer!`;
-        finalResult.textContent = "";
-    } else {
-        winner.textContent = `It's a tie!\nYou won the same amount of rounds as the computer!`;
-        finalResult.textContent = "";
-    }
+paperBtn.addEventListener("click", () => {
+    playRound("Paper");
+});
 
-    function updateUserScore() {
-        humanScore++;
+scissorsBtn.addEventListener("click", () => {
+    playRound("Scissors");
+});
+
+function resetScore() {
+    let decision = confirm("Do you want to play another round?");
+
+    if(decision) {
+        rounds = 0;
+        humanScore = 0;
+        computerScore = 0;
+
         userScoreDisplay.textContent = humanScore;
-    }
-    
-    function updateComputerScore() {
-        computerScore++;
         computerScoreDisplay.textContent = computerScore;
-    }
 
+        winnerText.style.visibility = "hidden";
+        winner.textContent = "";
+        finalResult.textContent = "";
+
+        humanIcon.className = "";
+        computerIcon.className ="fa-flip-horizontal";
+        
+        rockBtn.disabled = false;
+        paperBtn.disabled = false;
+        scissorsBtn.disabled = false;
+
+        rockBtn.style.cursor = "pointer";
+        paperBtn.style.cursor = "pointer";
+        scissorsBtn.style.cursor = "pointer";
+    } else {
+        alert("Thank you for playing");
+        window.close();
+    }
+}
+
+function disableButtons() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+
+    if(rockBtn.disabled == true) {
+        rockBtn.style.cursor = "auto";
+    }
+    if(paperBtn.disabled == true) {
+        paperBtn.style.cursor = "auto";
+    }
+    if(scissorsBtn.disabled == true) {
+        scissorsBtn.style.cursor = "auto";
+    }
+}
+
+function updateUserScore() {
+    humanScore++;
+    userScoreDisplay.textContent = humanScore;
+}
+
+function updateComputerScore() {
+    computerScore++;
+    computerScoreDisplay.textContent = computerScore;
 }
 
 
 
-playGame();
+
 
